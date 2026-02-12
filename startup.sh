abe477083267f81e075e0d69ec21a31814bf023d
0xqeew <cpqeew@gmail.com>
2026-02-12 08:39:43 +0100

    Stop running db setup during container startup

--- startup.sh
+++ startup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "🗄️  Setting up database..."
-npx prisma db push --accept-data-loss
-
-echo "🌱  Seeding database..."
-npx tsx prisma/seed.ts || true
-
 echo "🚀  Starting app..."
 exec npm start
